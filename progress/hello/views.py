ee2894605531bceac056924fc2b7b4c0b88caea3
Issah721 <johnymuthash96@gmail.com>
2025-10-16 19:12:28 +0300

    Added a description to index.html and greet.html

--- progress/hello/views.py
+++ progress/hello/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
 def index(request):
-    return render(request, "hello/index.html")
+    if request.method == "POST":
+        name = request.POST.get("name", "World") # Default to "World" if name is not provided
+        return HttpResponseRedirect(reverse("greet", args=[name]))
+    else:
+        return render(request, "hello/index.html")
 
 
 def Issah(request):
     return HttpResponse("Keep on going, my first django program written by myself.")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
